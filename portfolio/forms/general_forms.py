4ff3177c14974556efca2052526a1349b2c2b8c7
Ali Sirjani <alisirjani1234@gmail.com>
2024-02-16 17:09:35 +0330

    feat: Add admin for 'ContactMe' model
    Created the 'ContactMeAdmin' model admin to manage contact messages from users efficiently. The admin includes features to display, filter, and search contact messages based on various attributes like full name, subject, email, and answer status. Additionally, the admin prevents the addition of new contact messages directly.
    
    feat: Add 'ContactMeForm' model form
    
    Introduced the 'ContactMeForm' model form to facilitate the submission of contact messages. The form includes fields for the user's full name, email, subject, and message, allowing users to easily communicate their inquiries or feedback.

---
+++ portfolio/forms/general_forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from ..models import ContactMe
+
+
+class ContactMeForm(forms.ModelForm):
+    class Meta:
+        model = ContactMe
+        fields = ('full_name', 'email', 'subject', 'message')
