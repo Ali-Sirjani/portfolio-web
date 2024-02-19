dcf71098ac322cb85cef98b51738ca513e794b0f
Ali Sirjani <alisirjani1234@gmail.com>
2024-02-19 18:36:45 +0330

    refactor: Update context processor to optimize database queries
    Updated the context processor for recent posts to optimize database queries by using select_related and prefetch_related methods for related models.
    
    refactor: Customize widget and attributes in PostCommentForm
    
    Updated the PostCommentForm to customize the widget and attributes of the 'text' field. Now, the 'text' field in the form uses a Textarea widget with specified rows and a placeholder for user convenience.

--- portfolio/context_processors/blog_context_processors.py
+++ portfolio/context_processors/blog_context_processors.py
@@ -2,6 +2,7 @@ from ..models import Post
 
 
 def recent_posts(request):
-    recent_posts_query = Post.active_objs.all().order_by('-datetime_updated')[:3]
+    recent_posts_query = Post.active_objs.all().select_related('category').prefetch_related('images').order_by(
+        '-datetime_updated')[:3]
     context = {'recent_posts': recent_posts_query}
     return context
