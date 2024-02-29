24e564c5bae1ae0f168a080aa04d8159ef80a83d
Ali Sirjani <alisirjani1234@gmail.com>
2024-02-29 17:07:28 +0330

    feat: Add Profile Model
    Added the Profile model to store additional user information such as first name, last name, and profile picture. This model establishes a one-to-one relationship with the user model and allows for customizing user profiles within the application.
    
    feat: Override Email Field in CustomUser Model
    
    Overrode the email field inherited from the AbstractUser class in the CustomUser model to customize its behavior. The email field is now unique and can be left blank, allowing for more flexible user management and authentication.

--- core/models.py
+++ core/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+from portfolio.abstract import TimestampedModel
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, blank=True, verbose_name=_('email'))
+
+
+class Profile(TimestampedModel):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='profile',
+                                verbose_name=_('user'))
+
+    first_name = models.CharField(max_length=200, blank=True, verbose_name=_('First name'))
+    last_name = models.CharField(max_length=200, blank=True, verbose_name=_('Last name'))
+    picture = models.ImageField(upload_to='profile_pictures/',
+                                default='../static/images/logo-icon.png', blank=True,
+                                verbose_name=_('picture'))
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
+
+    def __str__(self):
+        return f'{self.user.username}'
