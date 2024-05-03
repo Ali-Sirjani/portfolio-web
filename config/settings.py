806445cd3a3b4663b690078105945e6c48baca12
Ali Sirjani <alisirjani1234@gmail.com>
2024-05-03 20:41:45 +0330

    chore: Remove dbbackup package from project
    Removed the dbbackup package from the project as it is no longer needed.

--- config/settings.py
+++ config/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
     'axes',
-    'dbbackup',
     'rosetta',
     'debug_toolbar',
     'mptt',
@@ -241,7 +240,3 @@ LOCALE_PATHS = (str(BASE_DIR.joinpath('templates/locale')),)
 # config crispy
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
-
-# config dbbackup
-DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
-DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'backup'}
